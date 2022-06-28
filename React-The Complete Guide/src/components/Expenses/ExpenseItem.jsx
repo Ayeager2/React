d99ffafd5cba1b8d88378a4fac283a1d8a8d8e42
Ayeager2 <annanyeager@gmail.com>
2022-06-28 15:55:33 -0400

    useState

--- React-The Complete Guide/src/components/Expenses/ExpenseItem.jsx
+++ React-The Complete Guide/src/components/Expenses/ExpenseItem.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from '../UI/Card/Card.jsx';
 import ExpenseDate from "./ExpenseDate.jsx";
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+
+
+    const [title, setTitle] = useState(props.title);
+
+    const clickHandler = () => {
+        setTitle('Updated');
+    }
+
     return (
         <Card className='expense-item' >
             <ExpenseDate date={ props.date } />
             <div className='expense-item__description'>
-                <h2>{ props.title }</h2>
+                <h2>{ title }</h2>
                 <div className='expense-item__price'>${ props.amount }</div>
             </div>
+            <button onClick={ clickHandler }>Edit</button>
         </Card >
     );
 }
