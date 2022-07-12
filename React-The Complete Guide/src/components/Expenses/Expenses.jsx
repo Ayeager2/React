4b34895c55f08e2de3f661f758256d5ba3f2ac07
Ayeager2 <annanyeager@gmail.com>
2022-07-12 13:58:15 -0400

    some clean up

--- React-The Complete Guide/src/components/Expenses/Expenses.jsx
+++ React-The Complete Guide/src/components/Expenses/Expenses.jsx
@@ -9,25 +9,30 @@ const Expenses = (props) => {
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear);
     }
+    const filterYearItems = props.items.filter(x => {
+        return x.date.getFullYear() == filteredYear
+    });
+
+    let expensesContent = <p>No Expenses Found.</p>;
+
+    if (filterYearItems.length > 0) {
+        expensesContent = filterYearItems.map((expense) => (
+            <ExpenseItem
+                key={ expense.id }
+                title={ expense.title }
+                amount={ expense.amount }
+                date={ expense.date }
+            />
+        ))
+    }
     return (
         <div>
             <Card className='expenses'>
-                <ExpensesFilter selected={ filteredYear } onFilterChange={ filterChangeHandler } />
-                <ExpenseItem
-                    title={ props.items[0].title }
-                    amount={ props.items[0].amount }
-                    date={ props.items[0].date }
+                <ExpensesFilter
+                    selected={ filteredYear }
+                    onFilterChange={ filterChangeHandler }
                 />
-                <ExpenseItem
-                    title={ props.items[1].title }
-                    amount={ props.items[1].amount }
-                    date={ props.items[1].date }
-                />{ " " }
-                <ExpenseItem
-                    title={ props.items[2].title }
-                    amount={ props.items[2].amount }
-                    date={ props.items[2].date }
-                />
+                { expensesContent }
             </Card>
         </div>
     );
