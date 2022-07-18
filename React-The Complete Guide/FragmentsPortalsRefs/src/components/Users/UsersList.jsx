0da05ba3b6001a500be347455849dba2e5493bed
Ayeager2 <annanyeager@gmail.com>
2022-07-18 12:23:37 -0400

    complete lesson

--- React-The Complete Guide/FragmentsPortalsRefs/src/components/Users/UsersList.jsx
+++ React-The Complete Guide/FragmentsPortalsRefs/src/components/Users/UsersList.jsx
@@ -5,13 +5,13 @@ import classes from './UsersList.module.css';
 
 const UsersList = (props) => {
   return (
-    <Card className={classes.users}>
+    <Card className={ classes.users }>
       <ul>
-        {props.users.map((user) => (
-          <li key={user.id}>
-            {user.name} ({user.age} years old)
+        { props.users.map((user) => (
+          <li key={ user.id }>
+            { user.name } ({ user.age } years old)
           </li>
-        ))}
+        )) }
       </ul>
     </Card>
   );
