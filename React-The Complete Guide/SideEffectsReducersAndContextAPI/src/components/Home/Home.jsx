13232db6a88809b1e78d2130cdc65573bc11a421
Ayeager2 <annanyeager@gmail.com>
2022-07-19 15:15:42 -0400

    react lunch work

--- React-The Complete Guide/SideEffectsReducersAndContextAPI/src/components/Home/Home.jsx
+++ React-The Complete Guide/SideEffectsReducersAndContextAPI/src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ import classes from './Home.module.css';
 
 const Home = (props) => {
   return (
-    <Card className={classes.home}>
+    <Card className={ classes.home }>
       <h1>Welcome back!</h1>
     </Card>
   );
